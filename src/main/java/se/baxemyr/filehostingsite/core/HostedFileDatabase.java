823bb7f0e6c643475aad64886c0cfd68db3bff67
anjonss <anjonss@gmail.com>
2012-10-16 17:59:10 +0200

    fixed queries to fit latest version

--- src/main/java/se/baxemyr/filehostingsite/core/HostedFileDatabase.java
+++ src/main/java/se/baxemyr/filehostingsite/core/HostedFileDatabase.java
@@ -20,7 +20,7 @@ public class HostedFileDatabase extends AbstractDAO<HostedFile, Long> {
 
     public List<HostedFile> getFiles(String name) {
         EntityManager em = super.emf.createEntityManager();
-        String file = "select f from UserHostedFile f where f.filename = :name";
+        String file = "select f from HostedFile f where f.filename = :name";
         TypedQuery<HostedFile> tq = em.createQuery(file, HostedFile.class);
         tq.setParameter("name", name);
         return tq.getResultList();
@@ -28,21 +28,21 @@ public class HostedFileDatabase extends AbstractDAO<HostedFile, Long> {
     
     public List<HostedFile> getFilesFromOwner(User owner) {
         EntityManager em = super.emf.createEntityManager();
-        String file = "select f from UserHostedFile f where f.owner = :owner";
+        String file = "select f from HostedFile f where f.owner = :owner";
         TypedQuery<HostedFile> tq = em.createQuery(file, HostedFile.class);
         tq.setParameter("owner", owner);
         return tq.getResultList();
     }
         public List<HostedFile> getLatestFiles() {
         EntityManager em = super.emf.createEntityManager();
-        String query = "select f from UserHostedFile f order by f.uploadDate desc";
+        String query = "select f from HostedFile f order by f.uploadDate desc";
         TypedQuery<HostedFile> tq = em.createQuery(query, HostedFile.class);
         return tq.getResultList();
     }
     
     public List<HostedFile> getMostDownloaded(){
         EntityManager em = super.emf.createEntityManager();
-        String query = "select f from UserHostedFile f order by f.downloads desc";
+        String query = "select f from HostedFile f order by f.downloads desc";
         TypedQuery<HostedFile> tq = em.createQuery(query, HostedFile.class);
         return tq.getResultList();
     }
