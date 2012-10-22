b2db84efe1db56eb7076855c9a2504fc714f67e2
phelerox <phelerox@129.16.234.171>
2012-10-23 00:09:15 +0200

    Det går nu att ändra namn (ej username), email och lösenord.

--- src/main/java/se/baxemyr/filehostingsite/logic/SettingsBB.java
+++ src/main/java/se/baxemyr/filehostingsite/logic/SettingsBB.java
@@ -6,12 +6,12 @@ package se.baxemyr.filehostingsite.logic;
 
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.*;
 import se.baxemyr.filehostingsite.core.AppUser;
 import se.baxemyr.filehostingsite.core.DatabaseManager;
@@ -22,13 +22,10 @@ import se.baxemyr.filehostingsite.core.SubjectGroup;
  *
  * @author anders
  */
-@Named("registerBB")
+@Named("settingsBB")
 @RequestScoped
-public class RegisterBB implements Serializable {
+public class SettingsBB implements Serializable {
    
-   @NotNull(message = "Can't be null")
-   @Pattern(regexp="\\p{Alpha}+", message="Only Alphabetic chars allowed")
-   private String username;
    @NotNull(message = "Can't be null")
    @Pattern(regexp="\\p{Alpha}+", message="Only Alphabetic chars allowed")
    private String name;
@@ -40,20 +37,22 @@ public class RegisterBB implements Serializable {
    private String repeatedPassword;
    
    private UserDatabase udb;
-   private static final Logger log = Logger.getLogger(RegisterBB.class.getName());
+   private static final Logger log = Logger.getLogger(SettingsBB.class.getName());
    
-   public RegisterBB(){  
+   public SettingsBB(){  
    }
      
    public String submit() throws IOException {
         try {
-            
+            FacesContext context = FacesContext.getCurrentInstance();
+            HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+            String username = request.getRemoteUser();
             udb = DatabaseManager.INSTANCE.getUserDatabase();
-            if (udb.find(username) == null) {
-                udb.add(new AppUser(username,name,email,password, SubjectGroup.USER));
-                return "/login/login?faces-redirect=true";
+            if (username != null) {
+                udb.update(new AppUser(username, name, email, password, SubjectGroup.USER));
+                return "/orderedLists?faces-redirect=true";
             }
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Username taken.", null));
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Are you logged in?", null));
             return null;
         } catch (Exception e) {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Error"));
@@ -61,15 +60,7 @@ public class RegisterBB implements Serializable {
             return null; // Same page
         }
 }
-   
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
+    
     public String getName() {
         return name;
     }
