a8972f025b60b3986e9de5f02386325fdf6198d8
anjonss <anjonss@gmail.com>
2012-10-23 23:40:36 +0200

    working with groups

--- src/main/java/se/baxemyr/filehostingsite/logic/DisplayGroupBB.java
+++ src/main/java/se/baxemyr/filehostingsite/logic/DisplayGroupBB.java
@@ -9,6 +9,7 @@ import javax.inject.Named;
 import se.baxemyr.filehostingsite.core.AppGroup;
 import se.baxemyr.filehostingsite.core.AppGroupDatabase;
 import se.baxemyr.filehostingsite.core.DatabaseManager;
+import se.baxemyr.filehostingsite.core.UserDatabase;
 
 /**
  *
@@ -22,11 +23,27 @@ import se.baxemyr.filehostingsite.core.DatabaseManager;
 public class DisplayGroupBB {
     private AppGroup group;
     private AppGroupDatabase groupDB = DatabaseManager.INSTANCE.getAppGroupDatabase();
+    private UserDatabase userDB = DatabaseManager.INSTANCE.getUserDatabase();
+    private String newUser;
     
     public DisplayGroupBB(){
         
     }
 
+    public String getNewUser() {
+        return newUser;
+    }
+
+    public void setNewUser(String newUser) {
+        this.newUser = newUser;
+    }
+
+    public void addNewMember(String id){
+        AppGroup tempGroup = groupDB.find(id);
+        tempGroup.addMember(userDB.find(newUser));
+        groupDB.update(group);
+    }
+    
     public AppGroup getGroup() {
         return group;
     }
